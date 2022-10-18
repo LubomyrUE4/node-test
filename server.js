733719375acb53c8e83701c259f1e61710813fb7
Liubomyr <liubomyr.protsyk@dotcode.pro>
2022-10-18 20:59:51 +0300

    new host

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(port, '0.0.0.0', () =>
+http.listen(port, () =>
   console.log(
     `Listening on port ${port}, process env = ${process.env.NODE_ENV}`
   )
